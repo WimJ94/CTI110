d18a52f9fd7026a64da4bf7cd5517cb70c93caa0
WimJ94 <wimberlj3704@student.faytechcc.edu>
2025-06-12 22:32:10 -0400

    Add files via upload
    First Upload

---
+++ P1LAB1_WimberlyJoshua.py
@@ -0,0 +1,12 @@
+#Wimberly Joshua
+#6/12/2025
+#P1LAB1
+#Here is a program that aloows you to input your first and last name
+
+First_name = input("First Name: ")
+Last_name = input("Last Name: ")
+comp_name = First_name + " " + Last_name 
+print("Hello",comp_name,"Welcome to CTI-110")
+
+
+
