815cba604272c21487fc73ab67875dd19341d85c
WimJ94 <wimberlj3704@student.faytechcc.edu>
2025-06-12 23:32:44 -0400

    Add files via upload
    first upload

--- P1HW1_WimberlyJoshua.py
+++ P1HW1_WimberlyJoshua.py
@@ -1,17 +1,19 @@
-print("-----Caculating Exponenets-----")
+print("-----Caculating Exponenets-----\n")
 
 
 base = input("Enter an integer as the base value: ")
 expon = input("Enter an integer as the exponent: ")
 final_value = int(base) ** int(expon)
+print()
 print(base, "Rasied to the power of", expon, "is", final_value)
 
 
-print("-----Addition and Subtraction-----")
+print("\n-----Addition and Subtraction-----\n")
 
 
 start = input("enter a starting integer: ")
 add = input("Enter an integer to add: ")
 sub = input("Enter an integer to subtract: ")
 value = int(start) + int(add) - int(sub)
+print()
 print(start, "+", add, "-", sub, "=", value)
