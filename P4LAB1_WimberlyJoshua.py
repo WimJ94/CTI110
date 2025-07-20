fcef964f8863dbd77bc023c8a6896f2cacf61529
WimJ94 <wimberlj3704@student.faytechcc.edu>
2025-07-20 05:18:15 -0400

    Add files via upload

---
+++ P4LAB1_WimberlyJoshua.py
@@ -0,0 +1,37 @@
+Wimberly Joshua
+#7/4/2025
+#P4LAB1
+#Uses turtle to draw my initials
+import turtle
+win = turtle.Screen()
+pen = turtle.Turtle()
+pen.pensize(5)
+pen.pencolor("violet")
+pen.shape("turtle")
+#for the J
+pen.forward(100)
+pen.right(180)
+pen.forward(50)
+pen.left(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(50)
+pen.right(90)
+pen.forward(30)
+#for the W
+pen.right(180)
+pen.forward(30)
+pen.left(90)
+pen.forward(50)
+pen.penup()
+pen.forward(70)
+pen.pendown()
+pen.left(120)
+pen.forward(100)
+pen.left(180)
+pen.forward(100)
+pen.left(120)
+pen.forward(50)
+pen.right(120)
+pen.forward(50)
+pen.left(120)
